3eeb64f7c63f16db3b8ac90ce25388a0dff74495
isadorapalhoti <isadora.lazaro.ext@dasa.com.br>
2022-03-24 15:30:20 -0300

    desenvolvimento inicial pronto ok

---
+++ src/main/kotlin/com/isadora/forum/dto/UsuarioDto.kt
@@ -0,0 +1,7 @@
+package com.isadora.forum.dto
+
+data class UsuarioDto(
+    val id: Long?,
+    val nome: String,
+    val email: String
+)
---
+++ src/main/kotlin/com/isadora/forum/mapper/UsuarioSaidaMapper.kt
@@ -0,0 +1,16 @@
+package com.isadora.forum.mapper
+
+import com.isadora.forum.dto.UsuarioDto
+import com.isadora.forum.model.Usuario
+import org.springframework.stereotype.Component
+
+@Component
+class UsuarioSaidaMapper: Mapper<Usuario,UsuarioDto> {
+    override fun map(t: Usuario): UsuarioDto {
+        return UsuarioDto(
+            id = t.id,
+            nome = t.nome,
+            email = t.email
+        )
+    }
+}
